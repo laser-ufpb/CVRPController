520a9b5a8db50aad42518d125e6e06bba01d5257
Bruno Passeti <brunopassetidev@gmail.com>
2020-08-02 09:00:00 -0300

    Code formatting

--- Controller.cpp
+++ Controller.cpp
@@ -1,57 +1,50 @@
+#include "model.h"
 #include <chrono>
 #include <cstdio>
 #include <cstring>
 #include <ctime>
-#include <signal.h>
-#include <unistd.h>
-#include <string>
 #include <iostream>
+#include <signal.h>
+#include <string>
+#include <unistd.h>
 #include <vector>
-#include "model.h"
 
 using namespace std;
-#define PATH_MAX 256
+#define PATH_MAX        256
 #define ARGUMENTS_COUNT 9
-#define BASE 2000
+#define BASE            2000
 
-FILE *fp;
+FILE* fp;
 
-void stopProces(int signal)
-{
+void stopProces(int signal) {
     int status = pclose(fp);
     printf("Process terminated\n");
 }
 
-bool parseLine(char *line, tSolution *sol)
-{
-    bool flag = false;
-    char *token = strtok(line, " ");
+bool parseLine(char* line, tSolution* sol) {
+    bool flag   = false;
+    char* token = strtok(line, " ");
 
-    if (strstr(token, "Route") != NULL)
-    {
-        vector<int> route;
+    if(strstr(token, "Route") != NULL) {
+        vector< int > route;
         token = strtok(NULL, " ");
         token = strtok(NULL, " ");
 
-        while (token != NULL)
-        {
+        while(token != NULL) {
             route.push_back(atoi(token));
             token = strtok(NULL, " ");
         }
         sol->routes.push_back(route);
-    }
-    else if (strstr(token, "Cost") != NULL)
-    {
-        token = strtok(NULL, " ");
+    } else if(strstr(token, "Cost") != NULL) {
+        token     = strtok(NULL, " ");
         sol->cost = atof(token);
-        flag = true;
+        flag      = true;
     }
 
     return flag;
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char* argv[]) {
     tData data(argc, argv);
     tInstance instance(data.path.c_str());
 
@@ -61,18 +54,18 @@ int main(int argc, char *argv[])
     typedef std::chrono::milliseconds milliseconds;
     char execCommand[PATH_MAX];
     sprintf(execCommand, "timeout %.2lfs %s", (data.baseTimeLimit / (data.passMark / BASE)), data.execCommand.c_str());
-    fp = popen( execCommand, "r");
+    fp = popen(execCommand, "r");
 
     Clock::time_point t0 = Clock::now();
 
     tSolution sol;
-    while (fgets(path, PATH_MAX, fp))
-    {
+    while(fgets(path, PATH_MAX, fp)) {
         Clock::time_point t1 = Clock::now();
-        milliseconds ms = std::chrono::duration_cast<milliseconds>(t1 - t0);
-        if (parseLine(path, &sol)){
+        milliseconds ms      = std::chrono::duration_cast< milliseconds >(t1 - t0);
+        if(parseLine(path, &sol)) {
             if(instance.checkInstance(sol))
-                cout << "Feasible Solution\n" << endl;
+                cout << "Feasible Solution\n"
+                     << endl;
         }
 
         fflush(stdout);
--- model.h
+++ model.h
@@ -2,15 +2,14 @@
 #define MODEL_H
 
 #include <iostream>
-#include <vector>
 #include <math.h>
-#include <string>
 #include <stdlib.h>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-struct tData
-{
+struct tData {
     string competitorName;
     string path;
     string execCommand;
@@ -21,43 +20,39 @@ struct tData
     int isRounded;
     int passMark;
 
-    tData(int argc, char *arguments[])
-    {
+    tData(int argc, char* arguments[]) {
         competitorName = string(arguments[1]);
-        path = string(arguments[2]);
-        isRounded = atoi(arguments[3]);
-        passMark = atoi(arguments[4]);
-        baseTimeLimit = atof(arguments[5]);
-        baseSolution = atof(arguments[6]);
-        bestSolution = atof(arguments[7]);
-        isOptimal = atoi(arguments[8]);
-        execCommand = string(arguments[9]);
-        for (int i = 10; i < argc; i++)
-        {
+        path           = string(arguments[2]);
+        isRounded      = atoi(arguments[3]);
+        passMark       = atoi(arguments[4]);
+        baseTimeLimit  = atof(arguments[5]);
+        baseSolution   = atof(arguments[6]);
+        bestSolution   = atof(arguments[7]);
+        isOptimal      = atoi(arguments[8]);
+        execCommand    = string(arguments[9]);
+        for(int i = 10; i < argc; i++) {
             execCommand += " " + string(arguments[i]);
         }
     }
-
 };
 
-struct tSolution{
-    vector<vector<int>> routes;
+struct tSolution {
+    vector< vector< int > > routes;
     double cost;
 };
 
-struct tInstance
-{
+struct tInstance {
     int dimension;
     string instanceName;
-    vector<int> demand;
-    vector<vector<int>> distanceMatrix;
+    vector< int > demand;
+    vector< vector< int > > distanceMatrix;
     int capacity;
 
-    tInstance(const char * instanciaPath){
-        FILE * file = fopen(instanciaPath, "r");
-    
-        if(!file){
-            std::cout<<"ERROR: Instance path wrong."<<std::endl;
+    tInstance(const char* instanciaPath) {
+        FILE* file = fopen(instanciaPath, "r");
+
+        if(!file) {
+            std::cout << "ERROR: Instance path wrong." << std::endl;
             exit(EXIT_FAILURE);
         }
 
@@ -69,55 +64,54 @@ struct tInstance
         fscanf(file, "DIMENSION : %d\n", &this->dimension);
         fscanf(file, "EDGE_WEIGHT_TYPE : %99[^\n]\n", &aux);
         fscanf(file, "CAPACITY : %d\n", &capacity);
-        demand = std::vector<int>(this->dimension, 0);
+        demand = std::vector< int >(this->dimension, 0);
         int lixo;
-        
+
         fscanf(file, "NODE_COORD_SECTION:\n");
-        this->distanceMatrix = std::vector< std::vector<int> >(dimension, std::vector<int>(dimension, 0));
-        std::vector<std::vector<int>> coord = std::vector<std::vector<int>>(dimension, std::vector<int>(3, 0)); 
-        
-        for(int i = 0; i < dimension; i++){
-            for(int j = 0; j < 3; j++){
-                fscanf(file,"%d", &coord[i][j]);
+        this->distanceMatrix                    = std::vector< std::vector< int > >(dimension, std::vector< int >(dimension, 0));
+        std::vector< std::vector< int > > coord = std::vector< std::vector< int > >(dimension, std::vector< int >(3, 0));
+
+        for(int i = 0; i < dimension; i++) {
+            for(int j = 0; j < 3; j++) {
+                fscanf(file, "%d", &coord[i][j]);
             }
         }
-        
+
         fscanf(file, "\n");
         fscanf(file, "DEMAND_SECTION:\n");
-        for(int i = 0; i < dimension; i++){
-            fscanf(file,"%d %d", &lixo, &this->demand[i]);
+        for(int i = 0; i < dimension; i++) {
+            fscanf(file, "%d %d", &lixo, &this->demand[i]);
         }
-        
-        for(int i = 0; i < dimension; i++){
-            for(int j = 0; j < dimension; j++){
-                this->distanceMatrix[i][j] = (int) round( sqrt(pow((coord[i][1] - coord[j][1]), 2) + pow((coord[i][2] - coord[j][2]), 2)));
-            }    
+
+        for(int i = 0; i < dimension; i++) {
+            for(int j = 0; j < dimension; j++) {
+                this->distanceMatrix[i][j] = (int)round(sqrt(pow((coord[i][1] - coord[j][1]), 2) + pow((coord[i][2] - coord[j][2]), 2)));
+            }
         }
 
         fclose(file);
     }
 
-    void printData(){
+    void printData() {
         printf("DIMENSION: %d\n", dimension);
 
         printf("CAPACITY: %d\n", capacity);
 
-        for(int i = 0; i < dimension; i++){
+        for(int i = 0; i < dimension; i++) {
             printf("DEMAND: %d %d\n", i, this->demand[i]);
         }
-        for(int i = 0; i < dimension; i++){
-            for(int j = 0; j < dimension; j++){
+        for(int i = 0; i < dimension; i++) {
+            for(int j = 0; j < dimension; j++) {
                 printf("%d ", distanceMatrix[i][j]);
             }
             puts("");
         }
     }
 
-    bool checkInstance(tSolution sol){
-        double solCost = 0;
+    bool checkInstance(tSolution sol) {
+        double solCost       = 0;
         int candidatesServed = 1;
-        for (vector<int> route : sol.routes)
-        {
+        for(vector< int > route : sol.routes) {
             candidatesServed += route.size();
             solCost += distanceMatrix[0][route.at(0)];
 
@@ -125,17 +119,15 @@ struct tInstance
             if(demanda > capacity)
                 return false;
 
-            for (int i = 1; i < route.size(); i++)
-            {
-                solCost += distanceMatrix[route.at(i-1)][route.at(i)];
+            for(int i = 1; i < route.size(); i++) {
+                solCost += distanceMatrix[route.at(i - 1)][route.at(i)];
 
                 if(demanda > capacity)
                     return false;
-
             }
             solCost += distanceMatrix[route.at(route.size() - 1)][0];
         }
-        
+
         if(solCost != sol.cost || candidatesServed < dimension)
             return false;
 
