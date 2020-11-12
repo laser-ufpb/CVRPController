1bc7dff1d9513732fe110dd8d8ab5f0d99ed755b
Bruno Passeti <32283944+brunopasseti@users.noreply.github.com>
2020-11-12 10:56:55 -0300

    Update genScript.sh

--- genScript.sh
+++ genScript.sh
@@ -1,22 +1,132 @@
-#!/bin/bash
-competitorName=$1
-passMark=$2
-solverPath=$3
-
-declare -A instancesBKS=([X-n101-k25.vrp]=27591 [X-n106-k14.vrp]=26362 [X-n110-k13.vrp]=14971 [X-n115-k10.vrp]=12747 [X-n120-k6.vrp]=13332 [X-n125-k30.vrp]=55539 [X-n129-k18.vrp]=28940 [X-n134-k13.vrp]=10916 [X-n139-k10.vrp]=13590 [X-n143-k7.vrp]=15700 [X-n148-k46.vrp]=43448 [X-n153-k22.vrp]=21220 [X-n157-k13.vrp]=16876 [X-n162-k11.vrp]=14138 [X-n167-k10.vrp]=20557 [X-n172-k51.vrp]=45607 [X-n176-k26.vrp]=47812 [X-n181-k23.vrp]=25569 [X-n186-k15.vrp]=24145 [X-n190-k8.vrp]=16980 [X-n195-k51.vrp]=44225 [X-n200-k36.vrp]=58578 [X-n204-k19.vrp]=19565 [X-n209-k16.vrp]=30656 [X-n214-k11.vrp]=10856 [X-n219-k73.vrp]=117595 [X-n223-k34.vrp]=40437 [X-n228-k23.vrp]=25742 [X-n233-k16.vrp]=19230 [X-n237-k14.vrp]=27042 [X-n242-k48.vrp]=82751 [X-n247-k50.vrp]=37274 [X-n251-k28.vrp]=38684 [X-n256-k16.vrp]=18839 [X-n261-k13.vrp]=26558 [X-n266-k58.vrp]=75478 [X-n270-k35.vrp]=35291 [X-n275-k28.vrp]=21245 [X-n280-k17.vrp]=33503 [X-n284-k15.vrp]=20215 [X-n289-k60.vrp]=95151 [X-n294-k50.vrp]=47161 [X-n298-k31.vrp]=34231 [X-n303-k21.vrp]=21736 [X-n308-k13.vrp]=25859 [X-n313-k71.vrp]=94043 [X-n317-k53.vrp]=78355 [X-n322-k28.vrp]=29834 [X-n327-k20.vrp]=27532 [X-n331-k15.vrp]=31102 [X-n336-k84.vrp]=139111 [X-n344-k43.vrp]=42050 [X-n351-k40.vrp]=25896 [X-n359-k29.vrp]=51505 [X-n367-k17.vrp]=22814 [X-n376-k94.vrp]=147713 [X-n384-k52.vrp]=65940 [X-n393-k38.vrp]=38260 [X-n401-k29.vrp]=66163 [X-n411-k19.vrp]=19712 [X-n420-k130.vrp]=107798 [X-n429-k61.vrp]=65449 [X-n439-k37.vrp]=36391 [X-n449-k29.vrp]=55233 [X-n459-k26.vrp]=24139 [X-n469-k138.vrp]=221824 [X-n480-k70.vrp]=89449 [X-n491-k59.vrp]=66487 [X-n502-k39.vrp]=69226 [X-n513-k21.vrp]=24201 [X-n524-k153.vrp]=154593 [X-n536-k96.vrp]=94868 [X-n548-k50.vrp]=86700 [X-n561-k42.vrp]=42717 [X-n573-k30.vrp]=50673 [X-n586-k159.vrp]=190316 [X-n599-k92.vrp]=108451 [X-n613-k62.vrp]=59535 [X-n627-k43.vrp]=62164 [X-n641-k35.vrp]=63694 [X-n655-k131.vrp]=106780 [X-n670-k130.vrp]=146332 [X-n685-k75.vrp]=68205 [X-n701-k44.vrp]=81923 [X-n716-k35.vrp]=43387 [X-n733-k159.vrp]=136190 [X-n749-k98.vrp]=77314 [X-n766-k71.vrp]=114454 [X-n783-k48.vrp]=72394 [X-n801-k40.vrp]=73305 [X-n819-k171.vrp]=158121 [X-n837-k142.vrp]=193737 [X-n856-k95.vrp]=88965 [X-n876-k59.vrp]=99299 [X-n895-k37.vrp]=53860 [X-n916-k207.vrp]=329179 [X-n936-k151.vrp]=132725 [X-n957-k87.vrp]=85465 [X-n979-k58.vrp]=118987 [X-n1001-k43.vrp]=72359 [Golden_9.vrp]=579.7 [Golden_10.vrp]=735.43 [Golden_11.vrp]=911.98 [Golden_12.vrp]=1101.5 [Golden_13.vrp]=857.19 [Golden_14.vrp]=1080.55 [Golden_15.vrp]=1337.27 [Golden_16.vrp]=1611.28 [Golden_17.vrp]=707.76 [Golden_18.vrp]=995.13 [Golden_19.vrp]=1365.6 [Golden_20.vrp]=1817.59 [E-n101-k8.vrp]=815 [E-n101-k14.vrp]=1067 [CMT4.vrp]=1028.42 [CMT5.vrp]=1291.29 [F-n135-k7.vrp]=1162 [tai385.vrp]=24366.41 [P-n101-k4.vrp]=681 [Antwerp1.vrp]=477306 [Antwerp2.vrp]=291400 [Brussels1.vrp]=501854 [Brussels2.vrp]=345565 [Flanders1.vrp]=7241290 [Flanders2.vrp]=4377626 [Ghent1.vrp]=469586 [Ghent2.vrp]=257958 [Leuven1.vrp]=192851 [Leuven2.vrp]=111489)
-declare -A instancesIsOptimal=([X-n101-k25.vrp]=1 [X-n106-k14.vrp]=1 [X-n110-k13.vrp]=1 [X-n115-k10.vrp]=1 	[X-n120-k6.vrp]=1 [X-n125-k30.vrp]=1 [X-n129-k18.vrp]=1 [X-n134-k13.vrp]=1 	[X-n139-k10.vrp]=1 	[X-n143-k7.vrp]=1 [X-n148-k46.vrp]=1 [X-n153-k22.vrp]=1 [X-n157-k13.vrp]=1 	[X-n162-k11.vrp]=1 	[X-n167-k10.vrp]=1 	[X-n172-k51.vrp]=1 	[X-n176-k26.vrp]=1 	[X-n181-k23.vrp]=1 	[X-n186-k15.vrp]=1 	[X-n190-k8.vrp]=1 	[X-n195-k51.vrp]=1 	[X-n200-k36.vrp]=1 	[X-n204-k19.vrp]=1 	[X-n209-k16.vrp]=1 	[X-n214-k11.vrp]=1 	[X-n219-k73.vrp]=1 	[X-n223-k34.vrp]=1 	[X-n228-k23.vrp]=1 	[X-n233-k16.vrp]=1 	[X-n237-k14.vrp]=1 	[X-n242-k48.vrp]=1 	[X-n247-k50.vrp]=1 	[X-n251-k28.vrp]=1 	[X-n256-k16.vrp]=0 	[X-n261-k13.vrp]=0 	[X-n266-k58.vrp]=1 	[X-n270-k35.vrp]=1 	[X-n275-k28.vrp]=1 	[X-n280-k17.vrp]=0 	[X-n284-k15.vrp]=1 	[X-n289-k60.vrp]=1 	[X-n294-k50.vrp]=0 	[X-n298-k31.vrp]=1 	[X-n303-k21.vrp]=0 	[X-n308-k13.vrp]=0 	[X-n313-k71.vrp]=0 	[X-n317-k53.vrp]=1 	[X-n322-k28.vrp]=1 	[X-n327-k20.vrp]=0 	[X-n331-k15.vrp]=1 	[X-n336-k84.vrp]=0 	[X-n344-k43.vrp]=0 	[X-n351-k40.vrp]=0 	[X-n359-k29.vrp]=0 	[X-n367-k17.vrp]=0 	[X-n376-k94.vrp]=1 	[X-n384-k52.vrp]=0 	[X-n393-k38.vrp]=1 	[X-n401-k29.vrp]=0 	[X-n411-k19.vrp]=0 	[X-n420-k130.vrp]=1 	[X-n429-k61.vrp]=0 	[X-n439-k37.vrp]=1 	[X-n449-k29.vrp]=0 	[X-n459-k26.vrp]=0 	[X-n469-k138.vrp]=1 	[X-n480-k70.vrp]=0 	[X-n491-k59.vrp]=0 	[X-n502-k39.vrp]=0 	[X-n513-k21.vrp]=0 	[X-n524-k153.vrp]=1 	[X-n536-k96.vrp]=0 	[X-n548-k50.vrp]=1 	[X-n561-k42.vrp]=0 	[X-n573-k30.vrp]=0 	[X-n586-k159.vrp]=0 	[X-n599-k92.vrp]=0 	[X-n613-k62.vrp]=0 	[X-n627-k43.vrp]=0 	[X-n641-k35.vrp]=0 	[X-n655-k131.vrp]=1 	[X-n670-k130.vrp]=0 	[X-n685-k75.vrp]=0 	[X-n701-k44.vrp]=0 	[X-n716-k35.vrp]=0 	[X-n733-k159.vrp]=0 	[X-n749-k98.vrp]=0 	[X-n766-k71.vrp]=0 	[X-n783-k48.vrp]=0 	[X-n801-k40.vrp]=0 	[X-n819-k171.vrp]=0 	[X-n837-k142.vrp]=0 	[X-n856-k95.vrp]=0 	[X-n876-k59.vrp]=0 	[X-n895-k37.vrp]=0 	[X-n916-k207.vrp]=0 [X-n936-k151.vrp]=0 [X-n957-k87.vrp]=0 	[X-n979-k58.vrp]=0 	[X-n1001-k43.vrp]=0 [Golden_9.vrp]=0 [Golden_10.vrp]=0 [Golden_11.vrp]=0 [Golden_12.vrp]=0 [Golden_13.vrp]=1 [Golden_14.vrp]=1 [Golden_15.vrp]=0 [Golden_16.vrp]=0 [Golden_17.vrp]=1 [Golden_18.vrp]=1 [Golden_19.vrp]=1 [Golden_20.vrp]=1 [Antwerp1.vrp]=0 [Antwerp2.vrp]=0 [Brussels1.vrp]=0 [Brussels2.vrp]=0 [Flanders1.vrp]=0 [Flanders2.vrp]=0 [Ghent1.vrp]=0 [Ghent2.vrp]=0 [Leuven1.vrp]=0 [Leuven2.vrp]=0 [E-n101-k8.vrp]=1 [E-n101-k14.vrp]=1 [CMT4.vrp]=1 [CMT5.vrp]=1 [F-n135-k7.vrp]=1 [tai385.vrp]=0 [P-n101-k4.vrp]=1)
-declare -A timeLimit=([X-n101-k25.vrp]=1800 [X-n106-k14.vrp]=1800 [X-n110-k13.vrp]=1800 [X-n115-k10.vrp]=1800 [X-n120-k6.vrp]=1800 [X-n125-k30.vrp]=1800 [X-n129-k18.vrp]=1800 [X-n134-k13.vrp]=1800 [X-n139-k10.vrp]=1800 [X-n143-k7.vrp]=1800 [X-n148-k46.vrp]=1800 [X-n153-k22.vrp]=1800 [X-n157-k13.vrp]=1800 [X-n162-k11.vrp]=1800 [X-n167-k10.vrp]=1800 [X-n172-k51.vrp]=1800 [X-n176-k26.vrp]=1800 [X-n181-k23.vrp]=1800 [X-n186-k15.vrp]=1800 [X-n190-k8.vrp]=1800 [X-n195-k51.vrp]=1800 [X-n200-k36.vrp]=1800 [X-n204-k19.vrp]=3600 [X-n209-k16.vrp]=3600 [X-n214-k11.vrp]=3600 [X-n219-k73.vrp]=3600 [X-n223-k34.vrp]=3600 [X-n228-k23.vrp]=3600 [X-n233-k16.vrp]=3600 [X-n237-k14.vrp]=3600 [X-n242-k48.vrp]=3600 [X-n247-k50.vrp]=3600 [X-n251-k28.vrp]=3600 [X-n256-k16.vrp]=3600 [X-n261-k13.vrp]=3600 [X-n266-k58.vrp]=3600 [X-n270-k35.vrp]=3600 [X-n275-k28.vrp]=3600 [X-n280-k17.vrp]=3600 [X-n284-k15.vrp]=3600 [X-n289-k60.vrp]=3600 [X-n294-k50.vrp]=3600 [X-n298-k31.vrp]=3600 [X-n303-k21.vrp]=3600 [X-n308-k13.vrp]=3600 [X-n313-k71.vrp]=3600 [X-n317-k53.vrp]=3600 [X-n322-k28.vrp]=3600 [X-n327-k20.vrp]=3600 [X-n331-k15.vrp]=3600 [X-n336-k84.vrp]=3600 [X-n344-k43.vrp]=3600 [X-n351-k40.vrp]=3600 [X-n359-k29.vrp]=3600 [X-n367-k17.vrp]=3600 [X-n376-k94.vrp]=3600 [X-n384-k52.vrp]=3600 [X-n393-k38.vrp]=3600 [X-n401-k29.vrp]=7200 [X-n411-k19.vrp]=7200 [X-n420-k130.vrp]=7200 [X-n429-k61.vrp]=7200 [X-n439-k37.vrp]=7200 [X-n449-k29.vrp]=7200 [X-n459-k26.vrp]=7200 [X-n469-k138.vrp]=7200 [X-n480-k70.vrp]=7200 [X-n491-k59.vrp]=7200 [X-n502-k39.vrp]=7200 [X-n513-k21.vrp]=7200 [X-n524-k153.vrp]=7200 [X-n536-k96.vrp]=7200 [X-n548-k50.vrp]=7200 [X-n561-k42.vrp]=7200 [X-n573-k30.vrp]=7200 [X-n586-k159.vrp]=7200 [X-n599-k92.vrp]=7200 [X-n613-k62.vrp]=7200 [X-n627-k43.vrp]=7200 [X-n641-k35.vrp]=7200 [X-n655-k131.vrp]=7200 [X-n670-k130.vrp]=7200 [X-n685-k75.vrp]=7200 [X-n701-k44.vrp]=7200 [X-n716-k35.vrp]=7200 [X-n733-k159.vrp]=7200 [X-n749-k98.vrp]=7200 [X-n766-k71.vrp]=7200 [X-n783-k48.vrp]=7200 [X-n801-k40.vrp]=7200 [X-n819-k171.vrp]=7200 [X-n837-k142.vrp]=7200 [X-n856-k95.vrp]=7200 [X-n876-k59.vrp]=7200 [X-n895-k37.vrp]=7200 [X-n916-k207.vrp]=7200 [X-n936-k151.vrp]=7200 [X-n957-k87.vrp]=7200 [X-n979-k58.vrp]=7200 [X-n1001-k43.vrp]=7200 [E-n101-k8.vrp]=1800 [E-n101-k14.vrp]=1800 [CMT4.vrp]=1800 [CMT5.vrp]=1800 [F-n135-k7.vrp]=1800 [P-n101-k4.vrp]=1800 [tai385.vrp]=3600 [Golden_9.vrp]=3600 [Golden_10.vrp]=3600 [Golden_11.vrp]=3600 [Golden_12.vrp]=7200 [Golden_13.vrp]=3600 [Golden_14.vrp]=3600 [Golden_15.vrp]=3600 [Golden_16.vrp]=7200 [Golden_17.vrp]=3600 [Golden_18.vrp]=3600 [Golden_19.vrp]=3600 [Golden_20.vrp]=7200 [Antwerp1.vrp]=7200 [Antwerp2.vrp]=7200 [Brussels1.vrp]=7200 [Brussels2.vrp]=7200 [Flanders1.vrp]=7200 [Flanders2.vrp]=7200 [Ghent1.vrp]=7200 [Ghent2.vrp]=7200 [Leuven1.vrp]=7200 [Leuven2.vrp]=7200)
-
-dir=InstancesRounded/
-
-for file in `ls ${dir}`
-do
-    echo ./build/CVRPController ${competitorName} ${dir}${file} 1 ${passMark} ${timeLimit[$file]} ${instancesBKS[$file]} ${instancesIsOptimal[$file]} ${solverPath}
-done
-
-dir=InstancesNotRounded/
-
-for file in `ls ${dir}`
-do
-    echo ./build/CVRPController ${competitorName} ${dir}${file} 0 ${passMark} ${timeLimit[$file]} ${instancesBKS[$file]} ${instancesIsOptimal[$file]} ${solverPath}
-done
+# $1: Competitor ID
+# $2: CPU mark
+# $3: Solver path
+echo ./build/CVRPController $1 InstancesRounded/Antwerp1.vrp 1 $2 7200 477306 0 $3
+echo ./build/CVRPController $1 InstancesRounded/Antwerp2.vrp 1 $2 7200 291400 0 $3
+echo ./build/CVRPController $1 InstancesRounded/Brussels1.vrp 1 $2 7200 501854 0 $3
+echo ./build/CVRPController $1 InstancesRounded/Brussels2.vrp 1 $2 7200 345565 0 $3
+echo ./build/CVRPController $1 InstancesRounded/E-n101-k14.vrp 1 $2 1800 1067 1 $3
+echo ./build/CVRPController $1 InstancesRounded/E-n101-k8.vrp 1 $2 1800 815 1 $3
+echo ./build/CVRPController $1 InstancesRounded/Flanders1.vrp 1 $2 7200 7241290 0 $3
+echo ./build/CVRPController $1 InstancesRounded/Flanders2.vrp 1 $2 7200 4377626 0 $3
+echo ./build/CVRPController $1 InstancesRounded/F-n135-k7.vrp 1 $2 1800 1162 1 $3
+echo ./build/CVRPController $1 InstancesRounded/Ghent1.vrp 1 $2 7200 469586 0 $3
+echo ./build/CVRPController $1 InstancesRounded/Ghent2.vrp 1 $2 7200 257958 0 $3
+echo ./build/CVRPController $1 InstancesRounded/Leuven1.vrp 1 $2 7200 192851 0 $3
+echo ./build/CVRPController $1 InstancesRounded/Leuven2.vrp 1 $2 7200 111489 0 $3
+echo ./build/CVRPController $1 InstancesRounded/P-n101-k4.vrp 1 $2 1800 681 1 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n1001-k43.vrp 1 $2 7200 72359 0 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n101-k25.vrp 1 $2 1800 27591 1 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n106-k14.vrp 1 $2 1800 26362 1 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n110-k13.vrp 1 $2 1800 14971 1 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n115-k10.vrp 1 $2 1800 12747 1 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n120-k6.vrp 1 $2 1800 13332 1 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n125-k30.vrp 1 $2 1800 55539 1 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n129-k18.vrp 1 $2 1800 28940 1 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n134-k13.vrp 1 $2 1800 10916 1 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n139-k10.vrp 1 $2 1800 13590 1 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n143-k7.vrp 1 $2 1800 15700 1 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n148-k46.vrp 1 $2 1800 43448 1 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n153-k22.vrp 1 $2 1800 21220 1 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n157-k13.vrp 1 $2 1800 16876 1 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n162-k11.vrp 1 $2 1800 14138 1 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n167-k10.vrp 1 $2 1800 20557 1 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n172-k51.vrp 1 $2 1800 45607 1 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n176-k26.vrp 1 $2 1800 47812 1 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n181-k23.vrp 1 $2 1800 25569 1 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n186-k15.vrp 1 $2 1800 24145 1 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n190-k8.vrp 1 $2 1800 16980 1 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n195-k51.vrp 1 $2 1800 44225 1 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n200-k36.vrp 1 $2 1800 58578 1 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n204-k19.vrp 1 $2 3600 19565 1 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n209-k16.vrp 1 $2 3600 30656 1 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n214-k11.vrp 1 $2 3600 10856 1 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n219-k73.vrp 1 $2 3600 117595 1 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n223-k34.vrp 1 $2 3600 40437 1 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n228-k23.vrp 1 $2 3600 25742 1 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n233-k16.vrp 1 $2 3600 19230 1 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n237-k14.vrp 1 $2 3600 27042 1 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n242-k48.vrp 1 $2 3600 82751 1 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n247-k50.vrp 1 $2 3600 37274 1 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n251-k28.vrp 1 $2 3600 38684 1 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n256-k16.vrp 1 $2 3600 18839 0 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n261-k13.vrp 1 $2 3600 26558 0 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n266-k58.vrp 1 $2 3600 75478 1 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n270-k35.vrp 1 $2 3600 35291 1 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n275-k28.vrp 1 $2 3600 21245 1 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n280-k17.vrp 1 $2 3600 33503 0 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n284-k15.vrp 1 $2 3600 20215 1 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n289-k60.vrp 1 $2 3600 95151 1 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n294-k50.vrp 1 $2 3600 47161 0 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n298-k31.vrp 1 $2 3600 34231 1 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n303-k21.vrp 1 $2 3600 21736 0 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n308-k13.vrp 1 $2 3600 25859 0 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n313-k71.vrp 1 $2 3600 94043 0 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n317-k53.vrp 1 $2 3600 78355 1 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n322-k28.vrp 1 $2 3600 29834 1 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n327-k20.vrp 1 $2 3600 27532 0 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n331-k15.vrp 1 $2 3600 31102 1 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n336-k84.vrp 1 $2 3600 139111 0 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n344-k43.vrp 1 $2 3600 42050 0 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n351-k40.vrp 1 $2 3600 25896 0 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n359-k29.vrp 1 $2 3600 51505 0 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n367-k17.vrp 1 $2 3600 22814 0 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n376-k94.vrp 1 $2 3600 147713 1 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n384-k52.vrp 1 $2 3600 65940 0 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n393-k38.vrp 1 $2 3600 38260 1 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n401-k29.vrp 1 $2 7200 66163 0 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n411-k19.vrp 1 $2 7200 19712 0 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n420-k130.vrp 1 $2 7200 107798 1 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n429-k61.vrp 1 $2 7200 65449 0 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n439-k37.vrp 1 $2 7200 36391 1 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n449-k29.vrp 1 $2 7200 55233 0 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n459-k26.vrp 1 $2 7200 24139 0 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n469-k138.vrp 1 $2 7200 221824 1 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n480-k70.vrp 1 $2 7200 89449 0 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n491-k59.vrp 1 $2 7200 66487 0 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n502-k39.vrp 1 $2 7200 69226 0 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n513-k21.vrp 1 $2 7200 24201 0 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n524-k153.vrp 1 $2 7200 154593 1 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n536-k96.vrp 1 $2 7200 94868 0 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n548-k50.vrp 1 $2 7200 86700 1 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n561-k42.vrp 1 $2 7200 42717 0 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n573-k30.vrp 1 $2 7200 50673 0 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n586-k159.vrp 1 $2 7200 190316 0 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n599-k92.vrp 1 $2 7200 108451 0 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n613-k62.vrp 1 $2 7200 59535 0 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n627-k43.vrp 1 $2 7200 62164 0 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n641-k35.vrp 1 $2 7200 63694 0 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n655-k131.vrp 1 $2 7200 106780 1 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n670-k130.vrp 1 $2 7200 146332 0 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n685-k75.vrp 1 $2 7200 68205 0 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n701-k44.vrp 1 $2 7200 81923 0 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n716-k35.vrp 1 $2 7200 43387 0 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n733-k159.vrp 1 $2 7200 136190 0 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n749-k98.vrp 1 $2 7200 77314 0 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n766-k71.vrp 1 $2 7200 114454 0 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n783-k48.vrp 1 $2 7200 72394 0 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n801-k40.vrp 1 $2 7200 73305 0 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n819-k171.vrp 1 $2 7200 158121 0 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n837-k142.vrp 1 $2 7200 193737 0 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n856-k95.vrp 1 $2 7200 88965 0 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n876-k59.vrp 1 $2 7200 99299 0 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n895-k37.vrp 1 $2 7200 53860 0 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n916-k207.vrp 1 $2 7200 329179 0 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n936-k151.vrp 1 $2 7200 132725 0 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n957-k87.vrp 1 $2 7200 85465 0 $3
+echo ./build/CVRPController $1 InstancesRounded/X-n979-k58.vrp 1 $2 7200 118987 0 $3
+echo ./build/CVRPController $1 InstancesNotRounded/CMT4.vrp 1 $2 1800 1028.42 1 $3
+echo ./build/CVRPController $1 InstancesNotRounded/CMT5.vrp 1 $2 1800 1291.29 1 $3
+echo ./build/CVRPController $1 InstancesNotRounded/Golden_10.vrp 1 $2 3600 735.43 0 $3
+echo ./build/CVRPController $1 InstancesNotRounded/Golden_11.vrp 1 $2 3600 911.98 0 $3
+echo ./build/CVRPController $1 InstancesNotRounded/Golden_12.vrp 1 $2 7200 1101.5 0 $3
+echo ./build/CVRPController $1 InstancesNotRounded/Golden_13.vrp 1 $2 3600 857.19 1 $3
+echo ./build/CVRPController $1 InstancesNotRounded/Golden_14.vrp 1 $2 3600 1080.55 1 $3
+echo ./build/CVRPController $1 InstancesNotRounded/Golden_15.vrp 1 $2 3600 1337.27 0 $3
+echo ./build/CVRPController $1 InstancesNotRounded/Golden_16.vrp 1 $2 7200 1611.28 0 $3
+echo ./build/CVRPController $1 InstancesNotRounded/Golden_17.vrp 1 $2 3600 707.76 1 $3
+echo ./build/CVRPController $1 InstancesNotRounded/Golden_18.vrp 1 $2 3600 995.13 1 $3
+echo ./build/CVRPController $1 InstancesNotRounded/Golden_19.vrp 1 $2 3600 1365.6 1 $3
+echo ./build/CVRPController $1 InstancesNotRounded/Golden_20.vrp 1 $2 7200 1817.59 1 $3
+echo ./build/CVRPController $1 InstancesNotRounded/Golden_9.vrp 1 $2 3600 579.7 0 $3
+echo ./build/CVRPController $1 InstancesNotRounded/tai385.vrp 1 $2 3600 24366.41 0 $3
